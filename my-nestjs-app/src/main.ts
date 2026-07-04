508140311fcca3bb3724f84941c92d0cd20bce8a
Nati Dogg <natnael.wondmu@gmail.com>
2026-07-04 07:00:04 -0700

    installed class-transformer and class-validator package

--- my-nestjs-app/src/main.ts
+++ my-nestjs-app/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+     new ValidationPipe({
+      whitelist: true, //strips properties that are not defined in our DTO's
+      forbidNonWhitelisted: true, // more strict than whitelist it immediately halts the request if extra properties detected
+      transform: true,
+      disableErrorMessages: true
+
+     })
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
