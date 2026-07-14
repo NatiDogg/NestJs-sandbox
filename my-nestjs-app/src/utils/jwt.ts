1dfdb3ef384f70c4c690106625623c8bbae96371
Nati Dogg <natnael.wondmu@gmail.com>
2026-07-14 06:27:47 -0700

    fixed my jwt.ts file to be a class

--- my-nestjs-app/src/utils/jwt.ts
+++ my-nestjs-app/src/utils/jwt.ts
@@ -6,9 +6,19 @@ import { envConfig } from './envValidation'
 
 @Injectable()
 export class TokenService{
-        constructor(private configService:ConfigService){}
-      createAccessToken(user: Omit<User, 'password'>){
-            return jwt.sign(user, this.configService.getOrThrow<string>("JWT_ACCESS_SECRET_KEY"), {expiresIn: '15m'})
+        constructor(private configService:ConfigService<envConfig>){}
+      createAccessToken(user: User){
+            return jwt.sign({id: user.id, name: user.name, email:user.email, role: user.role}, this.configService.getOrThrow<string>('JWT_ACCESS_SECRET_KEY'), {expiresIn: '15m'})
 
       }
+      createRefreshToken(user:User){
+         return jwt.sign({id: user.id, name: user.name, email:user.email, role: user.role}, this.configService.getOrThrow<string>("JWT_REFRESH_SECRET_KEY"), {expiresIn: '7d'})
+      }
+      verifyAccessToken(token: string){
+          return jwt.verify(token, this.configService.getOrThrow<string>('JWT_ACCESS_SECRET_KEY')) as {id: string, name: string, email: string, role: 'ADMIN' | "USER"}
+      }
+      verifyRefreshToken(token: string){
+          return jwt.verify(token, this.configService.getOrThrow<string>('JWT_REFRESH_SECRET_KEY')) as {id: string, name: string, email: string, role: 'ADMIN' | "USER"}
+      }
+
 }
